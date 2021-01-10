7aacf86124241438f141b050e674d7c88d9fbd9d
praveen143back <59838755+praveen143back@users.noreply.github.com>
2021-01-10 13:37:07 +0530

    Update question_5.py

--- question_5.py
+++ question_5.py
@@ -1,7 +1,9 @@
 def square(num):
+
     return num*num
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20]
+
 squares = map(square, numbers)
-squares_as_strings = map(str, squares)
-print(','.join(squares_as_strings))
+
+print(list(squares))
